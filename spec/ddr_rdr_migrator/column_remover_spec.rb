4fea18054dec77ddc57711ae43491e2959109df0
Jim Coble <jim.coble@duke.edu>
2018-08-02 11:01:12 -0400

    Remove columns not needed in migration; RDR-331

---
+++ spec/ddr_rdr_migrator/column_remover_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+module Ddr::IngestTools::DdrRdrMigrator
+
+  RSpec.describe ColumnRemover do
+
+    subject { described_class.new(manifest: manifest) }
+
+    let(:ddr_headers) do
+      [ 'pid', 'model', 'title', 'alternative', 'creator', 'contributor', 'affiliation', 'publisher', 'date',
+        'temporal', 'description', 'subject', 'spatial', 'language', 'type', 'format', 'relation', 'rights',
+        'provenance', 'citation', 'permanent_id', 'doi' ]
+    end
+    let(:rdr_headers) do
+      [ 'title', 'alternative', 'creator', 'contributor', 'affiliation', 'publisher', 'date', 'temporal',
+        'description', 'subject', 'spatial', 'language', 'type', 'format', 'relation', 'rights', 'provenance',
+        'citation', 'permanent_id', 'doi' ]
+    end
+    let(:ddr_data) do
+      [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v' ]
+    end
+    let(:rdr_data) do
+      [ 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v' ]
+    end
+    let(:manifest) { CSV::Table.new([ CSV::Row.new(ddr_headers, ddr_data) ]) }
+    let(:expected_manifest) { CSV::Table.new([ CSV::Row.new(rdr_headers, rdr_data) ]) }
+
+    it 'removes the appropriate manifest columns' do
+      expect(subject.call).to eq(expected_manifest)
+    end
+
+  end
+
+end
